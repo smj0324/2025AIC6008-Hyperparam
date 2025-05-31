ba950a1b4c71bf909a7037a9ba5d09ecd05576bf
minju seo <smj0324@oulook.com>
2025-05-31 20:07:44 +0900

    feat:repair train_tab

--- tuneparam/gui/theme/fonts.py
+++ tuneparam/gui/theme/fonts.py
@@ -1,4 +1,5 @@
 DEFAULT_FONT = ("나눔스퀘어_ac Bold", 11)
+DEFAULT_FONT_2 = ("나눔스퀘어_ac Bold", 9)
 ERROR_FONT_LARGE = ("나눔스퀘어_ac Bold", 30)
 ERROR_FONT = ("나눔스퀘어_ac Bold", 12)
 ERROR_FONT_SMALL = ("나눔스퀘어_ac Bold", 10)
